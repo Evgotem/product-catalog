b254474adc65b90a5a9b81b58a183ca410e35b5b
Temryazanskiy Evgeny <temryazanskiy.e@it-universe.ru>
2026-03-17 20:15:30 +0400

    refactor: delete text from header

--- src/modules/header/components/Header.tsx
+++ src/modules/header/components/Header.tsx
@@ -11,7 +11,7 @@ export const Header: React.FC<HeaderProps> = ({ onLogout }) => {
   return (
     <Layout.Header className={s.header}>
       <div className={s.logo}>
-        <span>Product Catalog</span>
+      
       </div>
       
       <div>
