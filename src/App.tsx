6a759531f9cae9a61a181b67d4fb5eac7dfbb864
Temryazanskiy Evgeny <temryazanskiy.e@it-universe.ru>
2026-03-17 19:08:50 +0400

    feat: App routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import { FC } from "react";
-import './index.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ConfigProvider } from 'antd';
+import ruRU from 'antd/locale/ru_RU';
+import { queryClient } from './shared/api/queryClient';
+// import { ProductsPage } from './pages/ProductsPage';
 import '@shared/styles/variables.css';
+import './index.css';
+import { LoginPage } from '@/pages';
 
-interface AppProps {
-
-}
-
-export const App: FC<AppProps> = () => {
-
+function App() {
   return (
-    <div>
-      <h1>Test</h1>
-    </div>
-  )
+    <QueryClientProvider client={queryClient}>
+      <ConfigProvider locale={ruRU}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/" element={<Navigate to="/login" replace />} />
+            {/* <Route path="/products" element={<ProductsPage />} /> */}
+          </Routes>
+        </BrowserRouter>
+      </ConfigProvider>
+    </QueryClientProvider>
+  );
 }
+
+export default App;
