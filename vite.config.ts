3c20fdad2f19082b20a35c8ebf4b7658ff6bf1d4
Temryazanskiy Evgeny <temryazanskiy.e@it-universe.ru>
2026-03-17 21:42:23 +0400

    fix: delete slash from base

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/product-catalog/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@modules': path.resolve(__dirname, './src/modules'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@shared': path.resolve(__dirname, './src/shared'),
+export default defineConfig(() => {
+
+  return {
+    plugins: [react()],
+    base: '/product-catalog',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '@pages': path.resolve(__dirname, './src/pages'),
+        '@modules': path.resolve(__dirname, './src/modules'),
+        '@components': path.resolve(__dirname, './src/components'),
+        '@shared': path.resolve(__dirname, './src/shared'),
+      },
     },
-  },
-  server: {
-    port: 3000,
-    open: true,
-  },
+    server: {
+      port: 3000,
+      open: true,
+    },
+  }
 })
